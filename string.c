fc9c8d370357ba6770739f27974cf3bba7c04106
Dwightun <roma556866@gmail.com>
2017-09-09 00:47:17 +0300

    qwe

--- string.c
+++ string.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
 
 #define N 1000
 void Split(char* string, char* delimiter, char*** tokens, int* tokensCount);
@@ -12,15 +13,22 @@ int main() {
 	int tokensCount;
 	string = (char*)malloc(sizeof(char)*N);
 	delimiter = (char*)malloc(sizeof(char)*N);
+	scanf("%s %s",string,delimiter);
+//	Split(string,delimiter,&tokens,&tokensCount);
 	for (int i = 0; i < strlen(string); i++)
 	{
-		tokens[i] = (char*)malloc(sizeof(char)*N);
+		printf("kf\n");
+		tokens = (char**)malloc(sizeof(char*)*100);
+		printf("i m gay3\n");
 	}
+	Split(string,delimiter,&tokens,&tokensCount);
+	printf("ferum\n");
 	for (int i = 0; i < tokensCount; i++)
 	{
-		printf("%s", tokens[i]);
+		printf("%s \n", tokens[i]);
 	}
-	system("pause");
+	printf("%d", tokensCount);
+	//system("pause");
 	return 0;
 }
 
@@ -28,17 +36,23 @@ void Split(char* string, char* delimiter, char*** tokens, int* tokensCount) {
 	int j=0;
 	char* string_word;
 	string_word = (char*)malloc(sizeof(char)*N);
+	printf("i m gay4\n");
 	for (int i = 0; i < strlen(string); i++)
 	{
+		printf("kick me\n");
 		if (strstr(string, delimiter) == NULL)
 			break;
 		else
 		{
 			string_word = strstr(string, delimiter);
+			printf("ku\n");
 			memcpy(tokens[j], string, strlen(string) - strlen(string_word));
+			printf("he\n");
 			j++;
 			string = string_word+strlen(delimiter);
+			printf("fuck you asshole\n");
 		}
 	}
-	tokensCount = j;
+	*tokensCount = j;
+	printf("fit\n");
 }
