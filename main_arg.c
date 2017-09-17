4c1fb9e9dc26c6b66a4ef3e870318dd3ebcd6786
Dwightun <roma556866@gmail.com>
2017-09-17 23:32:11 +0300

    1

--- main_arg.c
+++ main_arg.c
@@ -1,13 +1,71 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+//#include <unistd.h>
 
-int main(int argc,char *argv[],char *envp[]){
+#define N 1000
+
+void Split(char* string, char* delimiter, char** tokens, int* tokensCount);
+
+int main(){
+	char *text_of_comand_list;
 	int i = 0;
-	while (envp[i] != NULL)
-	{
-		printf("%s \n", envp[i]);
-		i++;
+	char **string_of_commands;
+	char **words_of_commands;
+	char string_delimiter = '\n';
+	char word_delimiter = ' ';
+	int count_of_commands = 0;
+	int *count_of_word_of_string_of_commands = 0;
+	FILE *comand_list = fopen("input.txt", "r");
+	if (comand_list == NULL) {
+		printf("file not exist");
+		exit(1);
 	}
-	execlp("pwd", "pwd", NULL);
+	else {
+		comand_list = (char*)malloc(sizeof(char*)*N);
+		text_of_comand_list = (char*)malloc(sizeof(char*)*N);
+		string_of_commands = (char**)malloc(sizeof(char*)*N);
+		words_of_commands = (char**)malloc(sizeof(char)*N);
+		//malloc 4x
+		fscanf(comand_list, &text_of_comand_list);
+		Split(text_of_comand_list, &string_delimiter, string_of_commands, &count_of_commands);
+		for (int i = 0; i < count_of_commands; i++) {
+			Split(&string_of_commands[i], &word_delimiter, words_of_commands, count_of_word_of_string_of_commands);
+		}
+		execv(words_of_commands[0], &words_of_commands, (char*)NULL);
+	}
+	FILE *f = fopen("output.txt", "w");
+	fprintf(f, "jopa");
+	fclose(f);
+	fclose(comand_list);
 	return 0;
+}
+
+void Split(char* string, char* delimiter, char** tokens, int* tokensCount) {
+	if (string == NULL || delimiter == NULL || tokens == NULL || tokensCount == NULL)
+	{
+		errno = "EINVAL";
+	}
+	else {
+		int j = 0;
+		char* string_word;
+		string_word = (char*)malloc(sizeof(char)*N);
+		strcat(string, delimiter);
+		for (int i = 0; i < strlen(string); i++)
+		{
+			if (strstr(string, delimiter) == NULL)
+				break;
+			else
+			{
+				string_word = strstr(string, delimiter);
+				strncpy(tokens[j], string, strlen(string) - strlen(string_word));
+				tokens[j][strlen(string) - strlen(string_word)] = '\0';
+				string = string_word + strlen(delimiter);
+				if (tokens[j][0] != 0)
+					j++;
+			}
+		}
+		*tokensCount = j;
+	}
 }
