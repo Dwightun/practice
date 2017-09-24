c10eff8c231d2748335c6e9186de59e701cff1b8
Dwightun <roma556866@gmail.com>
2017-09-24 19:57:32 +0300

    don't read this 2.0

--- commands/commands/main_arg.c
+++ commands/commands/main_arg.c
@@ -19,7 +19,6 @@ int main() {
 	FILE *command_list;
 	command_list = fopen("input.txt", "r+");
 	FILE *f = fopen("output.txt", "w");
-	printf("test\n");
 	if (command_list == NULL) {
 		printf("file not exist");
 		exit(1);
@@ -31,7 +30,7 @@ int main() {
 		word_delimiter[0] = ' ';
 		fseek(command_list, 0, SEEK_END);
 		long size_of_command_list = ftell(command_list);
-		printf("%ld \n", size_of_command_list);
+		//printf("%ld \n", size_of_command_list);
 		fseek(command_list, 0, SEEK_SET);
 		while (!feof(command_list))
 		{
@@ -43,8 +42,9 @@ int main() {
 		er = fread(text_of_command_list, sizeof(char), size_of_command_list, command_list);
 		//text_of_command_list[er] = '\n';
 		//text_of_command_list[er + 1] = '\0';
-		printf("%d \n", er);
-		printf("%s \n", text_of_command_list);
+		//printf("%d \n", er);
+		text_of_command_list[er] = '\0';
+		//printf("%s \n", text_of_command_list);
 		string_of_commands = (char**)calloc(N, sizeof(char*));
 		words_of_commands = (char***)calloc(N, sizeof(char**));
 		delay_time = (int**)calloc(count_of_strings_of_commands, sizeof(int*));
@@ -55,10 +55,11 @@ int main() {
 			words_of_commands[iterator] = (char**)calloc(N, sizeof(char*));
 		}
 		Split(text_of_command_list, string_delimiter, string_of_commands, &count_of_strings_of_commands);
+		/*
 		for (size_t i = 0; i < count_of_strings_of_commands; i++)
 		{
-			printf("%s \n", string_of_commands[i]);
-		}
+			printf("i=%d  %s \n",i, string_of_commands[i]);
+		}*/
 		count_of_word_of_string_of_commands = (int**)calloc(count_of_strings_of_commands, sizeof(int*));
 
 		for (size_t iterator = 0; iterator < (size_t)count_of_strings_of_commands; iterator++)
@@ -72,6 +73,7 @@ int main() {
 		for (size_t i = 0; i < (size_t)count_of_strings_of_commands; i++) {
 			Split(string_of_commands[i], word_delimiter, words_of_commands[i], count_of_word_of_string_of_commands[i]);
 		}
+		/*
 		for (size_t i = 0; i < (size_t)count_of_strings_of_commands; i++)
 		{
 			for (size_t j = 0; j < (size_t)count_of_word_of_string_of_commands[i][0]; j++)
@@ -79,22 +81,29 @@ int main() {
 				printf("%d = %d = %s ", i, j, words_of_commands[i][j]);
 			}
 			printf("\n");
-		}
+		}*/
 		for (size_t iterator = 0;  iterator < (size_t)count_of_strings_of_commands;  iterator++)
 		{
 			//execvp(words_of_commands[iterator][1], words_of_commands[iterator]);
 		}
 		//system("pause");
+		printf("goose\n");
+		
 		for (size_t iterator = 0; iterator < (size_t)count_of_strings_of_commands; iterator++)
 		{
+			printf("%d | = %s\n", iterator, words_of_commands[iterator][0]);
 			func(words_of_commands[iterator][0], delay_time[iterator]);
 			printf("%d\n", delay_time[iterator][0]);
 		}
+		
 		i = 0;
-		while (count_of_strings_of_commands > i)
+		printf("count_of_strings_of_commands  = %d", count_of_strings_of_commands );
+		while (count_of_strings_of_commands >= i)
 		{
+			printf("----- %s",words_of_commands[i][0]);
 			use_with_delay(words_of_commands[i], delay_time[0], count_of_word_of_string_of_commands[i][0]);
 			i++;
+			printf("__\n");
 		}
 		free(text_of_command_list);
 		free(string_delimiter);
