c38bbdb595e3827edfdaf53a6512d548a0665fe5
Vlad <vlad.miryaha@gmail.com>
2017-10-24 01:35:21 +0300

    fixit

--- homework/sem_3/Header.h
+++ homework/sem_3/Header.h
@@ -6,6 +6,11 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+/*
+fixit: из название файла неясно, что внутри него. 
+можно было бы создать два заголовочных файла. один - Utils.h со split'ом и конвертацией 
+времени, второй ExecuteCommand.h, c ф-ей ExecuteWithDelay
+*/
 
 void Split(char* string, char* delimiter, char** tokens, int* tokens_Count);
 void time_conv(char* string_of_data, int* delay_time);
